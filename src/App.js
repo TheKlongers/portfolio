27dc61b986a20386f09022a2a05a1a85b50daeb0
unknown <klingerribeiro@outlook.com>
2023-07-09 16:49:36 -0300

    Atualização 1.1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import capa from './img/bg-pattern-card.svg';
 import foto from './img/1620946670858.jpg';
+import foto from './img/calculadora.jpg';
 
 const projects = [
   {
     title: 'Calculadora',
     description: 'Desenvolvimento de calculadora simples utilizando JavaScript',
     link: 'https://calculadora-web-sooty.vercel.app/',
-    image: '',
+    image: './img/calculadora.jpg',
   },
   {
     title: 'Projeto 2',
