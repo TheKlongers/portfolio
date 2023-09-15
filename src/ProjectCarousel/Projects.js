e804e5b38b3c301ed466c0e1f52297db5db4ccf7
TheKlongers <116130625+TheKlongers@users.noreply.github.com>
2023-09-15 18:47:14 -0300

    Update Projects.js

--- src/ProjectCarousel/Projects.js
+++ src/ProjectCarousel/Projects.js
@@ -12,8 +12,8 @@ const projectsData = [
       gif: '/assets/project2.gif',
     },
     {
-      name: 'Lista de Tarefas',
-      link: 'https://lista-de-tarefas-beta-opal.vercel.app/',
+      name: 'Institucional - Imobiliária Santos',
+      link: 'https://mobiliaria.vercel.app/',
       gif: '/assets/project1.gif',
     },
     
